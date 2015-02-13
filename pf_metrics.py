25bff04d41d4d8742d290acc1daa9f3f5461a2a0
Renzo Massobrio <renzomassobrio@gmail.com>
2015-02-13 00:48:02 -0200

    fixed accent in comment causing encoding issues

--- pf_metrics.py
+++ pf_metrics.py
@@ -1,5 +1,5 @@
 # Copyright 2015 Renzo Massobrio
-# Facultad de Ingeniería, UdelaR
+# Facultad de Ingenieria, UdelaR
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
